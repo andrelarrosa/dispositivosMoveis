960dedd4cb30cad5939d6112b0ae7ef73a3c4072
andrelarrosa <andre_luiz_2001@hotmail.com>
2022-04-05 20:38:46 -0300

    variáveis

--- dart_application_1/lib/primeiro.dart
+++ dart_application_1/lib/primeiro.dart
@@ -1,3 +1,8 @@
 void funcao() {
-  print("Hello, World!");
+  String nomeProduto = "Chocolate";
+  double precoProduto = 2.20;
+  int quantidadeProduto = 3;
+  print("RELATÓRIO DE PRODUTOS");
+  print(
+      "Produto: ${nomeProduto}\nPreço: $precoProduto\nQuantidade: $quantidadeProduto");
 }
