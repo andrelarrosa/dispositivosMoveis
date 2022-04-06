c423d2fd8081a303df596e544b8a30664162f93e
andrelarrosa <andre_luiz_2001@hotmail.com>
2022-04-05 21:14:10 -0300

    dynamic

--- dart_application_1/lib/primeiro.dart
+++ dart_application_1/lib/primeiro.dart
@@ -1,8 +1,25 @@
+import 'dart:io';
+
 void funcao() {
   String nomeProduto = "Chocolate";
+  String entradaPreco = "3.55";
+  double preco = double.parse(entradaPreco);
   double precoProduto = 2.20;
+  String entradaQuantidade = "0x1E";
+  int quantidade = int.parse(entradaQuantidade);
   int quantidadeProduto = 3;
+  String? descricao; // precisa de variável null, coloca ?
+  dynamic dinamica = "vai menino";
+  dinamica = 213123123; // variável dinâmica
+
+  print("Informe seu nome:");
+  var a = stdin.readByteSync(); // scanner
+  print(a);
   print("RELATÓRIO DE PRODUTOS");
   print(
       "Produto: ${nomeProduto}\nPreço: $precoProduto\nQuantidade: $quantidadeProduto");
+  descricao = "====================";
+  print(descricao);
+  print("Produto: ${nomeProduto}\nPreço: $preco\nQuantidade: $quantidade");
+  print(descricao);
 }
