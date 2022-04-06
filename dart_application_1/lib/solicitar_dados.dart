cc858cba77191d4cfd9ef5b0d28a9db02b22146e
andrelarrosa <andre_luiz_2001@hotmail.com>
2022-04-05 21:51:22 -0300

    exercicio 1

---
+++ dart_application_1/lib/solicitar_dados.dart
@@ -0,0 +1,44 @@
+import 'dart:io';
+
+void solicitarDadosAluno() {
+  print("Informe o seu R.A");
+  String? raEntrada = stdin.readLineSync();
+  print("Informe o seu nome");
+  String? nomeEntrada = stdin.readLineSync();
+  print("Informe o seu ano de nascimento");
+  String? anoNascimentoEntrada = stdin.readLineSync()!;
+  print("Informe a sua serie");
+  String? serieEntrada = stdin.readLineSync()!;
+
+  int anoNascimento = int.parse(anoNascimentoEntrada);
+  int serie = int.parse(serieEntrada);
+
+  int idade = 2022 - anoNascimento;
+  print("=========================");
+  print(
+      " R.A: $raEntrada\n Nome: $nomeEntrada \n Idade: $idade \n Serie: $serie");
+  if (idade > 18) {
+    print(" Maioridade: Maior de Idade");
+  } else {
+    print(" Maioridade: Menor de Idade");
+  }
+  print("=========================");
+}
+
+void solicitarDadosDisciplina() {
+  print("Informe o ID da disciplina");
+  String? idEntrada = stdin.readLineSync();
+  print("Informe a carga horária da disciplina");
+  String? cargaHorariaEntrada = stdin.readLineSync()!;
+  print("Informe o nome da disciplina");
+  String? nomeDisciplinaEntrada = stdin.readLineSync();
+  print("Informe a serie da disciplina");
+  String? serieEntrada = stdin.readLineSync()!;
+
+  double cargaHoraria = double.parse(cargaHorariaEntrada);
+  double presencaMinima = cargaHoraria * 0.75;
+  print("=========================");
+  print(
+      " ID: $idEntrada\n Carga Horaria: $cargaHoraria \n Nome: $nomeDisciplinaEntrada \n Série: $serieEntrada");
+  print("=========================");
+}
--- dart_application_1/test/dart_application_1_test.dart
+++ dart_application_1/test/dart_application_1_test.dart
@@ -1,7 +1,9 @@
 // import 'package:dart_application_1/dart_application_1.dart';
 import 'package:dart_application_1/primeiro.dart' as primeiro;
+import 'package:dart_application_1/solicitar_dados.dart' as SolicitarDados;
 // import 'package:test/test.dart';
 
 void main() {
-  primeiro.funcao();
+  SolicitarDados.solicitarDadosAluno();
+  SolicitarDados.solicitarDadosDisciplina();
 }
