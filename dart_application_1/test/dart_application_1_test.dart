a10f34da0600b50b53c06bd750763c8ff36a272d
andrelarrosa <andre_luiz_2001@hotmail.com>
2022-04-10 01:05:17 -0300

    testes

--- dart_application_1/test/dart_application_1_test.dart
+++ dart_application_1/test/dart_application_1_test.dart
@@ -6,4 +6,16 @@ import 'package:dart_application_1/solicitar_dados.dart' as SolicitarDados;
 void main() {
   SolicitarDados.solicitarDadosAluno();
   SolicitarDados.solicitarDadosDisciplina();
+  String aprovacaoFaltas = SolicitarDados.verificarAprovacaoPorFaltas(75);
+  print(aprovacaoFaltas);
+  String media = SolicitarDados.verificarNotaAcimaDaMedia();
+  print(media);
+  SolicitarDados.calcularMediaMenorMaior();
+  SolicitarDados.verificarAprovacao(21);
+  SolicitarDados.verificarAprovacaoZeroADez();
+  SolicitarDados.solicitarDadosTresAlunos();
+  SolicitarDados.solicitarDadosVariosAlunos();
+  SolicitarDados.solicitarDadosDuasDisciplinas();
+  SolicitarDados.solicitarDisciplinas();
+  SolicitarDados.verificarAprovacaoAlunos();
 }
