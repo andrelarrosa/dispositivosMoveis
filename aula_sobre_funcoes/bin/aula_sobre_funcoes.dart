8eb0758b85ccf16e9141bdcf71799b3908fbf9f9
andrelarrosa <andre_luiz_2001@hotmail.com>
2022-04-12 20:01:06 -0300

    Aula de funções

---
+++ aula_sobre_funcoes/bin/aula_sobre_funcoes.dart
@@ -0,0 +1,17 @@
+import 'package:aula_sobre_funcoes/aula_sobre_funcoes.dart' as funcoes;
+import 'package:aula_sobre_funcoes/funcoes_com_retorno_com_parametro.dart'
+    as funcoesComRetornoComParametro;
+import 'package:aula_sobre_funcoes/funcoes_sem_retorno_com_parametro.dart'
+    as funcoesSemRetornoComParametro;
+import 'package:aula_sobre_funcoes/funcoes_sem_retorno_sem_parametro.dart'
+    as funcoesSemRetornoSemParametro;
+import 'package:aula_sobre_funcoes/funcoes_com_retorno_sem_parametro.dart'
+    as funcoesComRetornoSemParametro;
+
+void main(List<String> arguments) {
+  funcoes.show();
+  funcoesComRetornoComParametro.show();
+  funcoesSemRetornoComParametro.show();
+  funcoesSemRetornoSemParametro.show();
+  funcoesComRetornoSemParametro.show();
+}
